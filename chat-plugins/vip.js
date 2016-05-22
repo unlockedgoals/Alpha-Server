e2ab63105fd38dc7e34fa80b5b419f46cfe5a19e
Caleb Young <clbng44@gmail.com>
2016-05-22 03:15:28 +0000

    Setup: Fixes to VIP System

--- chat-plugins/vip.js
+++ chat-plugins/vip.js
@@ -8,6 +8,7 @@
 
 global.isVip = function (user) {
 	if (!user) return;
+	if (typeof user === 'Object') user = user.userid;
 	let vip = Db('vips').get(toId(user));
 	if (vip === 1) return true;
 	return false;
@@ -16,7 +17,7 @@ global.isVip = function (user) {
 exports.commands = {
 	vip: {
 		give: function (target, room, user) {
-			if (!this.can('eval')) return false;
+			if (!this.can('declare')) return false;
 			let vipUser = toId(target);
 			if (!vipUser) return this.parse('/help vip');
 			if (isVip(vipUser)) return this.errorReply(vipUser + ' is already a vip.');
@@ -24,7 +25,7 @@ exports.commands = {
 			this.sendReply(vipUser + ' has been granted with vip status.');
 		},
 		take: function (target, room, user) {
-			if (!this.can('eval')) return false;
+			if (!this.can('declare')) return false;
 			let vipUser = toId(target);
 			if (!vipUser) return this.parse('/help vip');
 			if (!isVip(vipUser)) return this.errorReply(vipUser + ' is not a vip.');
@@ -32,7 +33,7 @@ exports.commands = {
 			this.sendReply(vipUser + '\'s vip status has been taken.');
 		},
 		list: function (target, room, user) {
-			if (!this.can('eval')) return false;
+			if (!this.can('declare')) return false;
 			if (!Object.keys(Db('vips').object()).length) return this.errorReply('There seems to be no user with vip status.');
 			this.sendReplyBox('<center><b><u>Vip Users</u></b></center>' + '<br /><br />' + Object.keys(Db('vips').object()).join('<br />'));
 		},
